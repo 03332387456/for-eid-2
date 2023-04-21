f48c9130fab01d2d3239013cd0a0aaa835600bb2
03332387456 <sahirmavia@gmail.com>
2023-04-21 11:38:19 +0500

    this is for eid

---
+++ app.js
@@ -0,0 +1,11 @@
+let gender = prompt("Enter your gender");
+
+if (gender === "male") {
+  alert("Good Morning or evening sir ")
+  alert("EID MUBARAK ");
+} else if (gender === "female") {
+  alert("Good Morning or evening Ma'am")
+  alert("EID MUBARAK ");
+} else {
+  alert("Invalid input");
+}
